a9fe6ba82351a75aa9b13b485c07718e0c3d3b6d
EuanFairgrieve <euanfairgrieve@gmail.com>
2022-05-10 23:05:49 +0100

    Source file to dir in the archive file

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "recipes" {
 
 data "archive_file" "recipes" {
   type = "zip"
-  source_file = "${path.module}/markdown_table_gen"
+  source_dir = "${path.module}/markdown_table_gen"
   output_path = "${path.module}/${var.project}-latest.zip"
 }
 
